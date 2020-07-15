7ecb427915a0ebb1d712d9afbdd85909c8ace16c
Naoki Shibazaki <naokishibazaki@NaokinoMacBook-Pro.local>
2020-07-15 17:29:52 +0900

    imgsフォルダ追加

--- components/parts/AboutSection/index.vue
+++ components/parts/AboutSection/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>about</div>
+  <div id="about">
+    aboutooooo
+  <v-parallax
+    dark
+    src="~/assets/imgs/about01.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</div>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
