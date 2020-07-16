a0b04935035de02155dc28312af6cdb59e144bbc
sakurai00123 <zero2zero3zero4@gmail.com>
2020-07-16 15:32:03 +0900

    common

--- components/parts/worksSection/index.vue
+++ components/parts/worksSection/index.vue
@@ -21,7 +21,7 @@ export default {
     return{
       param1:'works',
       param2:'WORK',
-      param3: require("~/assets/imgs/about01.jpg"),
+      param3: require("~/assets/imgs/about.jpg"),
       param4:'人生は挑戦の連続家族の様に助け合い、友人の様に認め合い ライバルの様に高め合うそんな会社を目指しています。'
             + '遊びも仕事も楽しんだもん勝ち一人でも生きていける力を身につけよう',
       param5:'ながれちゃうー★彡'
