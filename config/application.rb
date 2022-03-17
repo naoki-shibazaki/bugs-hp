a832de0251755b03ba38249bf482a121c39d705a
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-03-17 17:47:39 +0900

    タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     # バリデーションの日本語化
     config.i18n.default_locale = :ja
 
