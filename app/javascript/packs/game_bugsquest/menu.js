a0c402fd6faa803c58cc7f73317dfebe1c224743
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-04-15 11:55:47 +0900

    メニューボタンの表示・非表示変更

--- app/javascript/packs/game_bugsquest/menu.js
+++ app/javascript/packs/game_bugsquest/menu.js
@@ -6,24 +6,30 @@ const nav2 = document.querySelector('nav2');
 
 if(btn){
   btn.addEventListener('click', () => {
+    if(btn2.innerHTML === '閉じる'){ return false; }
     nav.classList.toggle('open-menu')
-    // if (btn.innerHTML === 'メニュー') {
-    //   btn.innerHTML = '閉じる';
-    // } else {
-    //   btn.innerHTML = 'メニュー';
-    // }
-    // ↑ これと同じ意味の三項演算子での書き方 ↓
     btn.innerHTML = btn.innerHTML === 'メニュー'
-      ? '閉じる'
-      : 'メニュー'
+      ? (v => {menu_btn_change(2, false); menu_btn_change(3, false); return '閉じる';})()
+      : (v => {menu_btn_change(2, true); menu_btn_change(3, true); return 'メニュー';})()
   });
 }
 
 if(btn2){
   btn2.addEventListener('click', () => {
+    if(btn.innerHTML === '閉じる'){ return false; }
     nav2.classList.toggle('open-menu')
     btn2.innerHTML = btn2.innerHTML === 'アカウント'
-      ? '閉じる'
-      : 'アカウント'
+      ? (v => {menu_btn_change(1, false); menu_btn_change(3, false); return '閉じる';})()
+      : (v => {menu_btn_change(1, true); menu_btn_change(3, true); return 'アカウント';})()
   });
+}
+
+// メニューボタンの表示・非表示切り替え
+menu_btn_change = (btn_pos_num, show_flag) => {
+  const selecter = '.pulse-btn.position' + btn_pos_num
+  if(show_flag === true){
+    fadeProc(selecter, 0, 'In');
+  }else{
+    fadeProc(selecter, 0, 'Out');
+  }
 }
